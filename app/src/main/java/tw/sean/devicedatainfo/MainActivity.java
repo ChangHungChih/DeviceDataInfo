809893795bec316015b313a40a442254adcc7d65
Sean <seanchang@lafresh.com.tw>
2020-06-14 17:24:38 +0800

    add device info button and function

--- app/src/main/java/tw/sean/devicedatainfo/MainActivity.java
+++ app/src/main/java/tw/sean/devicedatainfo/MainActivity.java
@@ -6,15 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import java.util.List;
+import java.util.Map;
 
 import tw.sean.devicedatainfo.adapter.AppInfoAdapter;
 import tw.sean.devicedatainfo.model.AppInfo;
 import tw.sean.devicedatainfo.repository.DeviceData;
+import tw.sean.devicedatainfo.util.FormatUtil;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvData;
+    private ScrollView scrollView;
+    private TextView tvInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void findViews() {
         rvData = findViewById(R.id.rvData);
+        scrollView = findViewById(R.id.scrollView);
+        tvInfo = findViewById(R.id.tvInfo);
     }
 
     public void listApk(View view) {
@@ -33,6 +41,43 @@ public class MainActivity extends AppCompatActivity {
         rvData.setLayoutManager(new LinearLayoutManager(this));
         AppInfoAdapter appInfoAdapter = new AppInfoAdapter(appInfoList);
         rvData.setAdapter(appInfoAdapter);
+        rvData.setVisibility(View.VISIBLE);
+        scrollView.setVisibility(View.GONE);
+    }
+
+    public void showDeviceInfo(View view) {
+        rvData.setVisibility(View.GONE);
+        scrollView.setVisibility(View.VISIBLE);
+        String information = getDeviceInfoString();
+        tvInfo.setText(information);
+    }
+
+    private String getDeviceInfoString() {
+        Map<String, Long> storageData = DeviceData.getStorageInfo();
+        StringBuilder allInfo = new StringBuilder();
+        long totalStorageByte = storageData.get("total");
+        long availableStorageByte = storageData.get("available");
+        allInfo.append("***** Storage Information *****").append("\n");
+        allInfo.append("Total Storage:").append(FormatUtil.formatSize(totalStorageByte)).append("\n");
+        allInfo.append("Available Storage:").append(FormatUtil.formatSize(availableStorageByte)).append("\n");
+
+        Map<String, Long> memoryData = DeviceData.getMemoryInfo(this);
+        long totalMemoryByte = memoryData.get("total");
+        long availableMemoryByte = memoryData.get("available");
+        allInfo.append("\n").append("***** Memory Information *****").append("\n");
+        allInfo.append("Total Memory:").append(FormatUtil.formatSize(totalMemoryByte)).append("\n");
+        allInfo.append("Available Memory:").append(FormatUtil.formatSize(availableMemoryByte)).append("\n");
+
+        String cpuInfo = DeviceData.getCpuInfo();
+        allInfo.append("\n").append("***** CPU Information *****").append("\n");
+        allInfo.append(cpuInfo).append("\n");
+
+        Map<String, String> wifiAndMobileData = DeviceData.getWifiAndMobileInfo(this);
+        allInfo.append("\n").append("***** WiFi & Mobile data Information *****").append("\n");
+        allInfo.append("WiFi:").append(wifiAndMobileData.get("wifi")).append("\n");
+        allInfo.append("Mobile:").append(wifiAndMobileData.get("mobile")).append("\n");
+        allInfo.append("\n").append("\n").append("\n");
+        return allInfo.toString();
     }
 
 }
--- app/src/main/java/tw/sean/devicedatainfo/repository/DeviceData.java
+++ app/src/main/java/tw/sean/devicedatainfo/repository/DeviceData.java
@@ -1,15 +1,28 @@
 package tw.sean.devicedatainfo.repository;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Environment;
+import android.os.StatFs;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import tw.sean.devicedatainfo.model.AppInfo;
+
+import static android.content.Context.ACTIVITY_SERVICE;
 
 public class DeviceData {
 
@@ -34,6 +47,116 @@ public class DeviceData {
         }
 
         return appInfoList;
+    }
+
+    public static Map<String, Long> getStorageInfo() {
+        Map<String, Long> data = new HashMap<>();
+        File path = Environment.getDataDirectory();
+        StatFs statFs = new StatFs(path.getPath());
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            data.put("total", statFs.getTotalBytes());
+            data.put("available", statFs.getAvailableBytes());
+        } else {
+            int blockSize = statFs.getBlockSize();
+            long blockCount = statFs.getBlockCount();
+            long availBlockCount = statFs.getAvailableBlocks();
+            data.put("total", blockSize * blockCount);
+            data.put("available", blockSize * availBlockCount);
+        }
+
+        return data;
+
+    }
+
+    public static Map<String, Long> getMemoryInfo(Context context) {
+        ActivityManager actManager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memInfo = new ActivityManager.MemoryInfo();
+        Map<String, Long> data = new HashMap<>();
+        if (actManager != null) {
+            actManager.getMemoryInfo(memInfo);
+        }
+        data.put("total", memInfo.totalMem);
+        data.put("available", memInfo.availMem);
+
+        return data;
+    }
+
+    public static String getMemoryDetailInfo() {
+        ProcessBuilder cmd;
+        StringBuilder result = new StringBuilder();
+
+        try {
+            String[] args = {"/system/bin/cat", "/proc/meminfo"};
+            cmd = new ProcessBuilder(args);
+
+            Process process = cmd.start();
+            InputStream in = process.getInputStream();
+            byte[] re = new byte[1024];
+            while (in.read(re) != -1) {
+                result.append(new String(re));
+            }
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+    public static String getCpuInfo() {
+        ProcessBuilder cmd;
+        StringBuilder result = new StringBuilder();
+
+        try {
+            String[] args = {"/system/bin/cat", "/proc/cpuinfo"};
+            cmd = new ProcessBuilder(args);
+
+            Process process = cmd.start();
+            InputStream in = process.getInputStream();
+            byte[] re = new byte[1024];
+            while (in.read(re) != -1) {
+                result.append(new String(re));
+            }
+            in.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return result.toString();
+    }
+
+    public static Map<String, String> getWifiAndMobileInfo(Context context) {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] networkInfoArray;
+        HashMap<String, String> map = new HashMap<>();
+        if (connectivityManager != null) {
+            networkInfoArray = connectivityManager.getAllNetworkInfo();
+            for (NetworkInfo networkInfo : networkInfoArray) {
+                if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                    map.put("wifi", networkInfo.toString());
+                }
+                if (networkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                    map.put("mobile", networkInfo.toString());
+                }
+            }
+        }
+        return map;
+    }
+
+    public static String[] getWifiInfo(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext()
+                .getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = null;
+        if (wifiManager != null) {
+            wifiInfo = wifiManager.getConnectionInfo();
+        }
+        String wifiString = null;
+        if (wifiInfo != null) {
+            wifiString = wifiInfo.toString();
+        } else {
+            wifiString = "";
+        }
+        return wifiString.split(",");
     }
 
 }
